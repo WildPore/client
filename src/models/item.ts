6fa66ae4d084a9de6283d406705f821dc3a20ee4
Lewis Parnell <lewis.g.parnell@gmail.com>
2023-07-10 17:34:54 -0700

    Revert item change immediately; cause issues with linting

--- src/models/item.ts
+++ src/models/item.ts
@@ -1,12 +1,6 @@
-// TODO I'm adding null to the types of id and name.
-// I'm doing this so that I can initialize a state with Item
-// by saying that these properties are null.
-// I don't know if that's the best choice. What trade-off am
-// I making?
-
 export interface Item {
-	id: number | null;
-	name: string | null;
+	id: number;
+	name: string;
 	maxHeld?: number;
 	ordering?: number;
 }
