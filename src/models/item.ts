d328cd71343e73cff54d91ce9156a3c12bfeffdc
Lewis Parnell <lewis.g.parnell@gmail.com>
2023-07-10 17:32:43 -0700

    Extend item model for state initialization

--- src/models/item.ts
+++ src/models/item.ts
@@ -1,6 +1,12 @@
+// TODO I'm adding null to the types of id and name.
+// I'm doing this so that I can initialize a state with Item
+// by saying that these properties are null.
+// I don't know if that's the best choice. What trade-off am
+// I making?
+
 export interface Item {
-	id: number;
-	name: string;
+	id: number | null;
+	name: string | null;
 	maxHeld?: number;
 	ordering?: number;
 }
